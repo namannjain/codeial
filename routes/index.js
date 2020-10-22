0642a3c63953277982831d550b2535ba357239e2
namannjain <namanjain8302@gmail.com>
2020-10-22 15:06:37 +0530

    setup static files for layouts and pages

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ console.log("Router loaded");
 
 
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
 
+//for any further routes, access from here
+// router.use('/routerName', require('./routerFile'));
 
 module.exports = router;
